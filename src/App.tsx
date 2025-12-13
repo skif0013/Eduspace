acde2998db9fc7a1594b70e017d7ff80c8c27571
Daniil Choffel <daniilmiadzve@gmail.com>
2025-12-14 00:24:36 +0100

    Revert "first try train commit Implement Login/SignUP"
    This reverts commit fa20f7a19ea355fc2b924ebd8c019c84e7ae7361.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { LoginPage } from './pages/auth/LoginPage';
-import { SignUpPage } from './pages/auth/SignUpPage';
-import { MainPage } from './pages/MainPage';
-import authService from './services/authService';
-
-const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return authService.isAuthenticated() ? <>{children}</> : <Navigate to="/login" />;
-};
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route
-                    path="/main"
-                    element={
-                        <PrivateRoute>
-                            <MainPage />
-                        </PrivateRoute>
-                    }
-                />
-                <Route path="/" element={<Navigate to="/login" />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
