95ab253ea366f7177ed83e6672410e03c2c4e5c8
Daniil Choffel <daniilmiadzve@gmail.com>
2026-01-18 23:37:37 +0100

    feat(infrastructure): add PostgreSQL persistence

--- QuizService.Infrastructure/Persistence/Data/ApplicationDbContext.cs
+++ QuizService.Infrastructure/Persistence/Data/ApplicationDbContext.cs
@@ -13,4 +13,31 @@ public class ApplicationDbContext : DbContext
     
     public DbSet<AnswerOption> AnswerOptions { get; set; }
     
+    
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Quiz>(entity =>
+        {
+            entity.HasKey(q => q.QuizId);
+            entity.Property(q => q.Name).IsRequired().HasMaxLength(200);
+            entity.Property(q => q.Description).HasMaxLength(1000);
+            entity.HasMany(q => q.Questions).WithOne().OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Question>(entity =>
+        {
+            entity.HasKey(q => q.Id);
+            entity.Property(q => q.Text).IsRequired();
+            entity.HasMany<AnswerOption>().WithOne().OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<AnswerOption>(entity =>
+        {
+            entity.HasKey(a => a.Id);
+            entity.Property(a => a.Text).IsRequired();
+        });
+    }
 }
