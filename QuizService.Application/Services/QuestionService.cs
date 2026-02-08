36e613d44c0359aa289357d9e38297667727db1d
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-08 15:29:27 +0100

    feat: add QuestionMapper and QuestiionService implementations

---
+++ QuizService.Application/Services/QuestionService.cs
@@ -0,0 +1,103 @@
+﻿using QuizService.Application.Contracts;
+using QuizService.Application.Contracts.QuestionsContract;
+using QuizService.Application.DTOs.QuestionsDTOs.CreateRequestDTOs;
+using QuizService.Application.DTOs.QuestionsDTOs.ResponseDTO;
+using QuizService.Application.Repositories;
+using QuizService.Domain.Models;
+
+
+namespace QuizService.Application.Services;
+
+public class QuestionService : IQuestionService
+{
+    
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IQuestionRepository _questionRepository;
+    private readonly IQuestionMapper _questionMapper;
+    
+    public QuestionService( IUnitOfWork unitOfWork, IQuestionRepository questionRepository, IQuestionMapper questionMapper)
+    {
+        _questionMapper = questionMapper;
+        _unitOfWork = unitOfWork;
+        _questionRepository = questionRepository;
+    }
+
+    public async Task<QuestionResponseDTO> AddQuestionToQuizAsync(CreateQuestionRequestDTO requestDTO)
+    {
+        var question = new Question
+        {
+            Id = Guid.NewGuid(),
+            Text = requestDTO.Text,
+            Order = requestDTO.Order,
+            MaxScore = requestDTO.MaxScore,
+            QuestionType = requestDTO.QuestionType,
+            IsActive = true,
+            QuizId = requestDTO.QuizId
+        };
+        
+        
+        await _questionRepository.AddQuestion(question);
+        await _unitOfWork.SaveChangesAsync();
+        
+        return _questionMapper.ToResponse(question);
+    }
+
+
+    public Task<QuestionResponseDTO> GetQuestionByIdAsync(Guid id)
+    {
+        var find = _questionRepository.FindByIdAsync(id);
+        
+        if (find == null)
+        {
+            throw new Exception("Question not found");
+        }
+        
+        return Task.FromResult(_questionMapper.ToResponse(find.Result));
+    }
+
+    public async Task<IReadOnlyCollection<QuestionResponseDTO>> GetAllQuestionsAsync()
+    {
+        var questions = await _questionRepository.GetAllQuestionsAsync();
+
+        return questions
+            .Select(_questionMapper.ToResponse)
+            .ToList();
+    }
+
+    public async Task<bool> DeleteQuestionFromQuizAsync(Guid id)
+    {
+        var find = await _questionRepository.FindByIdAsync(id);
+        
+        if (find == null)
+        {
+            throw new Exception("Question not found");
+        }
+        
+        await _questionRepository.RemoveAsync(find);
+        await _unitOfWork.SaveChangesAsync();
+        
+        return true;
+    }
+    
+    
+    public async Task<QuestionResponseDTO> UpdateQuestionToQuizAsync(CreateQuestionRequestDTO requestDTO, Guid questionId)
+    {
+        var find = await _questionRepository.FindByIdAsync(questionId);
+        
+        if (find == null)
+        {
+            throw new Exception("Question not found");
+        }
+        
+        find.Text = requestDTO.Text;
+        find.Order = requestDTO.Order;
+        find.MaxScore = requestDTO.MaxScore;
+        find.QuestionType = requestDTO.QuestionType;
+        find.IsActive = true;
+        find.QuizId = requestDTO.QuizId;
+        
+        await _unitOfWork.SaveChangesAsync();
+        
+        return _questionMapper.ToResponse(find);
+    }
+}
---
+++ QuizService.Infrastructure/Persistence/Repositories/QuestionRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizService.Application.Contracts.QuestionsContract;
+using QuizService.Domain.Models;
+using QuizService.Infrastructure.Data;
+
+namespace QuizService.Infrastructure.Repositories;
+
+public class QuestionRepository : IQuestionRepository
+{
+    private readonly ApplicationDbContext _context;
+    
+    public QuestionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    
+    public async Task<IReadOnlyCollection<Question>> GetAllQuestionsAsync() =>
+        await _context.Questions.ToListAsync();
+    
+    public  Task AddQuestion(Question question)
+    {
+        _context.Questions.Add(question);
+        
+        return Task.CompletedTask;
+    }
+
+
+    public async Task<Question?> FindByIdAsync(Guid questionId)
+    {
+        return await _context.Questions.FirstOrDefaultAsync(q => q.Id == questionId);
+    }
+        
+
+    public Task RemoveAsync(Question question)
+    {
+        _context.Remove(question);
+        
+        return Task.CompletedTask;
+    }
+}
