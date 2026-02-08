8edb907b4250363c232c29c477f0410e3e5d4bec
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-08 15:23:59 +0100

    feat: add Question mapper

---
+++ QuizService.Application/Contracts/Mappers/IQuestionMapper.cs
@@ -0,0 +1,9 @@
+﻿using QuizService.Application.DTOs.QuestionsDTOs.ResponseDTO;
+using QuizService.Domain.Models;
+
+namespace QuizService.Application.Contracts;
+
+public interface IQuestionMapper
+{
+    QuestionResponseDTO ToResponse(Question question);
+}
