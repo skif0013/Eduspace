28fb2f8de59fe963a69ec2f8141b515879409156
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-08 15:25:44 +0100

    feat: add QuestionMapper implementation

---
+++ QuizService.Application/Mapper/QuestionMapper.cs
@@ -0,0 +1,24 @@
+﻿using QuizService.Application.Contracts;
+using QuizService.Application.DTOs.QuestionsDTOs.ResponseDTO;
+using QuizService.Domain.Models;
+
+namespace QuizService.Application.Services;
+
+public class QuestionMapper : IQuestionMapper
+{
+    public QuestionResponseDTO ToResponse(Question question)
+    {
+        return new QuestionResponseDTO
+        {
+            QuestionId = question.Id,
+            Text = question.Text,
+            Order = question.Order,
+            MaxScore = question.MaxScore,
+            QuestionType = question.QuestionType,
+            IsActive = question.IsActive
+,            QuizId = question.QuizId
+        };
+    }
+    
+    
+}
