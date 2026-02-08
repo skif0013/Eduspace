81480a55ac708fa1c93a5b5d658a5cafec8e053b
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-08 15:27:37 +0100

    feat: add question service and repository contracts

---
+++ QuizService.Application/Contracts/QuestionsContract/IQuestionRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuizService.Domain.Models;
+
+namespace QuizService.Application.Contracts.QuestionsContract;
+
+public interface IQuestionRepository
+{
+    Task<IReadOnlyCollection<Question>> GetAllQuestionsAsync();
+    
+    Task<Question?> FindByIdAsync(Guid questionId);
+    
+    Task AddQuestion(Question question);
+    
+    Task RemoveAsync(Question question);
+}
---
+++ QuizService.Application/Contracts/QuestionsContract/IQuestionService.cs
@@ -0,0 +1,18 @@
+﻿using QuizService.Application.DTOs.QuestionsDTOs.CreateRequestDTOs;
+using QuizService.Application.DTOs.QuestionsDTOs.ResponseDTO;
+using QuizService.Application.DTOs.QuestionsDTOs.UpdateDTOs;
+
+namespace QuizService.Application.Contracts.QuestionsContract;
+
+public interface IQuestionService
+{
+    Task<QuestionResponseDTO> AddQuestionToQuizAsync(CreateQuestionRequestDTO requestDTO);
+    
+    Task<QuestionResponseDTO> UpdateQuestionToQuizAsync(CreateQuestionRequestDTO requestDto, Guid questionId);
+    
+    Task<bool> DeleteQuestionFromQuizAsync(Guid questionId);
+    
+    Task<QuestionResponseDTO> GetQuestionByIdAsync(Guid questionId);
+    
+    Task<IReadOnlyCollection<QuestionResponseDTO>> GetAllQuestionsAsync();
+}
