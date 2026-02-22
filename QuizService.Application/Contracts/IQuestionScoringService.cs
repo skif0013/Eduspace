28aa6f8055fc6580de08f51ebffbd282defc1cfa
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-22 02:32:37 +0100

    feat(application): add scoring contracts and submit DTOs

---
+++ QuizService.Application/Contracts/IQuestionScoringService.cs
@@ -0,0 +1,9 @@
+﻿using QuizService.Application.DTOs.QuizDTOs.SubmitDTOs;
+using QuizService.Domain.Models;
+
+namespace QuizService.Application.Contracts;
+
+public interface IQuestionScoringService
+{
+    double CalculateScore(Question question, IReadOnlyCollection<Guid> selectedOptionIds, bool isCorrect = false);
+}
