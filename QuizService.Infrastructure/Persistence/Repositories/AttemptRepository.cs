e0213eb00212edec26f89a5479f8b9040d5af937
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-27 22:13:22 +0100

    feat(repository): add AttemptRepository and contract

---
+++ QuizService.Infrastructure/Persistence/Repositories/AttemptRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizService.Application.Contracts.IQuizAttempt;
+using QuizService.Domain.Enum;
+using QuizService.Domain.Models;
+using QuizService.Infrastructure.Data;
+
+namespace QuizService.Infrastructure.Repositories;
+
+public class AttemptRepository : IAttemptRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public AttemptRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(QuizAttempt attempt)
+    {
+        await _context.QuizAttempts.AddAsync(attempt);
+    }
+
+    public Task<QuizAttempt?> GetByIdAsync(Guid attemptId)
+    {
+        return _context.QuizAttempts
+            .FirstOrDefaultAsync(a => a.Id == attemptId);
+    }
+
+    public Task<QuizAttempt?> GetWithAnswersAsync(Guid attemptId)
+    {
+        return _context.QuizAttempts
+            .Include(a => a.Answers)
+            .FirstOrDefaultAsync(a => a.Id == attemptId);
+    }
+
+    public Task<bool> HasActiveAttemptAsync(Guid quizId, Guid userId)
+    {
+        return _context.QuizAttempts
+            .AnyAsync(a =>
+                a.QuizId == quizId &&
+                a.UserId == userId &&
+                a.Status == AttemptStatus.InProgress);
+    }
+}
