4cf0422806363fec36ac1fe0f9a176d7c15c31fa
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-27 22:13:04 +0100

    feat(domain): add QuizAttempt and UserAnswer entities with AttemptStatus enum

---
+++ QuizService.Domain/Models/QuizAttempt.cs
@@ -0,0 +1,25 @@
+﻿using QuizService.Domain.Enum;
+
+namespace QuizService.Domain.Models;
+
+public class QuizAttempt
+{
+    public Guid Id { get; set; }
+
+    public Guid QuizId { get; set; }
+    
+    public Quiz Quiz { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public DateTime StartedAt { get; set; }
+    
+    public DateTime? FinishedAt { get; set; }
+
+    public int Score { get; set; }
+
+    public AttemptStatus Status { get; set; }
+
+    public ICollection<UserAnswer> Answers { get; set; } = new List<UserAnswer>();
+
+}
---
+++ QuizService.Domain/Models/UserAnswer.cs
@@ -0,0 +1,18 @@
+﻿namespace QuizService.Domain.Models;
+
+public class UserAnswer
+{
+    public Guid Id { get; set; }
+
+    public Guid AttemptId { get; set; }
+    
+    public QuizAttempt Attempt { get; set; }
+
+    public Guid QuestionId { get; set; }
+
+    public string? TextAnswer { get; set; }
+
+    public Guid? SelectedOptionId { get; set; }
+
+    public bool IsCorrect { get; set; }
+}
