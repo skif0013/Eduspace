a5f19eca2406b06a4a3ef62a4f12d4a1ec00b902
Daniil Choffel <daniilmiadzve@gmail.com>
2026-03-01 00:40:50 +0100

    chore:delete method

--- QuizService.WebApi/Controllers/QuizController.cs
+++ QuizService.WebApi/Controllers/QuizController.cs
@@ -18,55 +18,47 @@ public class QuizController : ControllerBase
         _tokenService = tokenService;
         _quizService = quizService;
     }
-    
+
     [HttpPost("Create")]
     public async Task<ActionResult<QuizResponseDTO>> Create([FromBody] CreatingQuizRequestDTO request)
     {
         // var userId = _tokenService.GetUserIdFromToken(token);
-        
+
         var result = await _quizService.CreateQuizAsync(request);
         return CreatedAtAction(nameof(GetById), new { id = result.QuizId }, result);
     }
 
-    
+
     [HttpGet("GetById")]
     public async Task<ActionResult<QuizResponseDTO>> GetById([FromRoute] Guid quizId, Guid userId)
     {
-        var get = await _quizService.GetQuizByIdAsync(quizId,userId);
-        
+        var get = await _quizService.GetQuizByIdAsync(quizId, userId);
+
         return Ok(get);
     }
 
-    
+
     [HttpGet("getAll")]
     public async Task<ActionResult<IReadOnlyCollection<QuizResponseDTO>>> GetAll()
     {
         var result = await _quizService.GetAllQuizzesAsync();
         return Ok(result);
     }
-    
-    
+
+
     [HttpPut("Update {quizId}")]
-    public async Task<IActionResult> Update([FromRoute] Guid quizId,[FromBody] QuizUpdateRequestDTO request)
+    public async Task<IActionResult> Update([FromRoute] Guid quizId, [FromBody] QuizUpdateRequestDTO request)
     {
         await _quizService.UpdateQuizAsync(quizId, request);
-        
+
         return NoContent();
     }
-    
+
     [HttpDelete("Delete{quizId}]")]
     public async Task<IActionResult> Delete([FromRoute] Guid quizId)
     {
         await _quizService.DeleteQuizAsync(quizId);
-        
+
         return NoContent();
-    }
-    
-    [HttpPost("submit")]
-    public async Task<ActionResult<QuizSubmitResponseDTO>> SubmitQuiz([FromBody] SubmitQuizRequestDTO request)
-    {
-        var result = await _quizService.SubmitQuizAsync(request);
-        
-        return Ok(result);
     }
 }
