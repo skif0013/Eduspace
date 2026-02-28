c2f87729eec4a26c5ffb7703da8597f5d50f433a
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-28 17:40:43 +0100

    feat(user-answer): add repository and implementation for user answers

---
+++ QuizService.Application/Contracts/IUserAnswerRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuizService.Domain.Models;
+
+namespace QuizService.Application.Contracts;
+
+public interface IUserAnswerRepository
+{
+    Task<UserAnswer> AddAsync(UserAnswer userAnswer);
+    
+    Task<IReadOnlyCollection<UserAnswer>> GetByAttemptIdAsync(Guid attemptId);
+    
+    Task RemoveAsync(UserAnswer userAnswer);
+    
+    Task<UserAnswer?> GetByIdAsync(Guid userAnswerId);
+}
