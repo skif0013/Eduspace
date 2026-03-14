a03c5604f44f236cd9c6bab2365a6c1ed5928237
Choffel <daniilmiadzve@gmail.com>
2026-03-14 04:36:16 +0300

    refactor: refactor controller for file service

--- src/FileService/FileService.WebApi/Controllers/FilesController.cs
+++ src/FileService/FileService.WebApi/Controllers/FilesController.cs
@@ -1,54 +1,62 @@
 ﻿using System.Security.Claims;
-using FileService.Application.Service;
+using AutoMapper;
+using FileService.Application.Contracts.Repositories;
+using FileService.Application.DTOs.BlobDTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileService.WebApi.Controllers;
 
+
+
+//[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class FilesController : ControllerBase
 {
-    private readonly BlobStorageService _blobService;
+    private readonly IFileService _fileService;
+   
     
     
-    public FilesController(BlobStorageService blobService)
+    public FilesController(IFileService fileService)
     {
-        _blobService = blobService;
+        _fileService = fileService;
     }
 
     [HttpPost("upload")]
     [Consumes("multipart/form-data")]
-    public async Task<IActionResult> UploadFile(IFormFile file)
+    public async Task<ActionResult<FileResponse>> UploadFile([FromForm] UploadFileRequest request, CancellationToken ct)
     {
-        var userIdFromClaims = User.FindFirst("userId")?.Value;
-        Guid userId = Guid.Parse(userIdFromClaims);
+        Guid fakeUserId = Guid.Parse("00000000-0000-0000-0000-000000000000");
         
-        using var stream = file.OpenReadStream();
-
-        var result = await _blobService.UploadFileAsync(stream, file.FileName, userId);
-
+        var result = await _fileService.UploadAsync(request,fakeUserId,ct);
+        
         return Ok(result);
-    }
-    
-    [HttpGet("{fileId}/link")]
-    public async Task<IActionResult> GetFileLink(Guid fileId)
-    {
-        var userIdFromClaims = User.FindFirst("userId")?.Value;
-        Guid userId = Guid.Parse(userIdFromClaims);
-        
-        var url = await _blobService.GetFileLinkAsync(fileId, userId);
-        
-        return Ok(url);
     }
     
     [HttpDelete("{fileId}")]
     public async Task<IActionResult> DeleteFile(Guid fileId)
     {
-        var userIdFromClaims = User.FindFirst("userId")?.Value;
-        Guid userId = Guid.Parse(userIdFromClaims);
         
-        await _blobService.DeleteFileAsync(fileId, userId);
+        Guid fakeUserId = Guid.Parse("00000000-0000-0000-0000-000000000000");
+        
+        await _fileService.DeleteAsync(fileId,fakeUserId);
         
         return NoContent();
+    }
+
+    [HttpPut("{fileId:guid}/update")] 
+    [Consumes("multipart/form-data")]
+    public async Task<ActionResult<FileResponse>> UpdateFile(
+        [FromForm] UpdateFileRequest request,
+        [FromRoute] Guid fileId, 
+        CancellationToken ct)
+    {
+        Guid fakeUserId = Guid.Parse("00000000-0000-0000-0000-000000000000");
+        
+        
+        var response = await _fileService.UpdateContentAsync(request, fileId, fakeUserId, ct);
+    
+        // 3. Возвращаем именно то, что пришло из сервиса
+        return Ok(response);
     }
 }
