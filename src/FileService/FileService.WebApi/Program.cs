924288ca80d4804f15a75e4d4328f1fb41ba74dc
Choffel <daniilmiadzve@gmail.com>
2026-03-15 21:42:58 +0300

    feat(webapi): register file service in DI and update startup

--- src/FileService/FileService.WebApi/Program.cs
+++ src/FileService/FileService.WebApi/Program.cs
@@ -1,13 +1,10 @@
-using System.Text;
 using Azure.Storage.Blobs;
 using FileService.Application.Contracts.Repositories;
 using FileService.Application.Mapper;
 using FileService.Application.Service;
 using FileService.Infrastructure.Data;
 using FileService.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,69 +15,17 @@ builder.WebHost.ConfigureKestrel(options =>
     options.ListenAnyIP(5011);
 });
 
-
-#region config jwt
-var validIssuer = Environment.GetEnvironmentVariable("AuthServiceJwt__Issuer");
-var validAudience = Environment.GetEnvironmentVariable("AuthServiceJwt__Audience");
-var symmetricSecurityKey = Environment.GetEnvironmentVariable("AuthServiceJwt__Key");
-
-builder.Services.AddAuthentication(options => {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.IncludeErrorDetails = true;
-        options.TokenValidationParameters = new TokenValidationParameters()
-        {
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = validIssuer,
-            ValidAudience = validAudience,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(symmetricSecurityKey)
-            ),
-        };
-    });
-#endregion
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "File Service API", Version = "v1" });
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Description = "Enter JWT Bearer token: Bearer {token}",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
-    c.AddSecurityDefinition("Bearer", securityScheme);
-
-    var securityRequirement = new OpenApiSecurityRequirement
-    {
-        { securityScheme, new[] { "Bearer" } }
-    };
-    c.AddSecurityRequirement(securityRequirement);
 });
 
 builder.Services.AddSingleton(x =>
     new BlobServiceClient(builder.Configuration["AzureStorage:ConnectionString"])
 );
 
-// закинуть данные азура 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (string.IsNullOrEmpty(connectionString))
 {
@@ -92,9 +37,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddAuthorization();
 
+
+
 builder.Services.AddAutoMapper(typeof(FileMappingProfile).Assembly);
-builder.Services.AddScoped<BlobStorageService>();
+builder.Services.AddScoped<IBlobService,BlobStorageService>();
 builder.Services.AddScoped<IFileRepository, FileRepository>();
+builder.Services.AddScoped<IFileService, FileService.Application.Service.FileService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
@@ -116,7 +64,6 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
