4f267c1bce26e7fb5e4dfd7bd12516e8d1cfdc24
Choffel <daniilmiadzve@gmail.com>
2026-03-22 18:29:55 +0300

    feat(file-service): add contracts for new endpoint

--- src/FileService/FileService.Application/DTOs/BatchFileLinksResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿using FileService.Application.DTOs.BlobDTOs;
-
-namespace FileService.Application.DTOs;
-
-public record BatchFileLinksResponse(IReadOnlyCollection<FileResponse> Files, IReadOnlyCollection<Guid> NotFoundFileIds);
