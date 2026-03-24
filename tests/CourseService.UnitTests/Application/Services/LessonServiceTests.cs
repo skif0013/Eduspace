c841a55534337b62948b15e30d09323b3c94b34e
AndriySham <avshamatrin@gmail.com>
2026-03-24 18:27:44 +0200

    Add tests LessonService/CresteLesson

--- tests/CourseService.UnitTests/Application/Services/LessonServiceTests.cs
+++ tests/CourseService.UnitTests/Application/Services/LessonServiceTests.cs
@@ -2,6 +2,7 @@
 using CourseService.Application.Caching;
 using CourseService.Application.Courses.DTO;
 using CourseService.Application.Courses.Errors;
+using CourseService.Application.Courses.Events;
 using CourseService.Application.Courses.Interfaces;
 using CourseService.Application.Courses.Services;
 using CourseService.Application.Messaging;
@@ -11,6 +12,7 @@ using CourseService.Domain.Enums;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System.Security.AccessControl;
 using Xunit;
 
 namespace CourseService.UnitTests.Application.Services;
@@ -39,6 +41,169 @@ public class LessonServiceTests
             _publisherMock.Object,
             _keyBuilderMock.Object
         );
+    }
+
+    [Fact]
+    public async Task CreateLessonAsync_ShouldReturnFailure_WhenCourseNotFound()
+    {
+        var courseId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+
+        var lessonDto = new LessonDTO();
+
+        _courseRepositoryMock
+            .Setup(x => x.GetCourseByIdAsync(courseId))
+            .ReturnsAsync((Course?)null);
+
+        // Act
+        var result = await _lessonService.CreateLessonAsync(lessonDto, courseId, authorId);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(CourseErrors.CourseNotFound);
+
+        _courseRepositoryMock.Verify(x => x.GetCourseByIdAsync(courseId), Times.Once);
+
+        _mapperMock.Verify(x => x.Map<Lesson>(It.IsAny<LessonDTO>), Times.Never);
+        _lessonRepositoryMock.Verify(x => x.CreateLessonAsync(It.IsAny<Lesson>()), Times.Never);
+        _mapperMock.Verify(x => x.Map<LessonResponse>(It.IsAny<Lesson>()), Times.Never);
+        _publisherMock.Verify(x => x.PublishAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        _cacheMock.Verify(x => x.RemoveAsync(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CreateLessonAsync_ShouldReturnFailure_WhenNotCourseAuthor()
+    {
+        // Arrange
+        var courseId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+
+        var lessonDto = new LessonDTO();
+        var course = new Course
+        {
+            Id = courseId,
+            AuthorId = Guid.NewGuid(),
+            Status = CourseStatus.Published
+        };
+
+        _courseRepositoryMock
+            .Setup(x => x.GetCourseByIdAsync(courseId))
+            .ReturnsAsync(course);
+
+        // Act
+        var result = await _lessonService.CreateLessonAsync(lessonDto, courseId, authorId);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(CourseErrors.NotCourseAuthor);
+
+        _courseRepositoryMock.Verify(x => x.GetCourseByIdAsync(courseId), Times.Once);
+
+        _mapperMock.Verify(x => x.Map<Lesson>(It.IsAny<LessonDTO>()), Times.Never);
+        _lessonRepositoryMock.Verify(x => x.CreateLessonAsync(It.IsAny<Lesson>()), Times.Never);
+        _mapperMock.Verify(x => x.Map<LessonResponse>(It.IsAny<Lesson>()), Times.Never);
+        _publisherMock.Verify(x => x.PublishAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        _cacheMock.Verify(x => x.RemoveAsync(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CreateLessonAsync_ShouldReturnFailure_WhenCourseArchived()
+    {
+        // Arrange
+        var courseId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+
+        var lessonDto = new LessonDTO();
+        var course = new Course
+        {
+            Id = courseId,
+            AuthorId = authorId,
+            Status = CourseStatus.Archived
+        };
+
+        _courseRepositoryMock
+            .Setup(x => x.GetCourseByIdAsync(courseId))
+            .ReturnsAsync(course);
+
+        // Act
+        var result = await _lessonService.CreateLessonAsync(lessonDto, courseId, authorId);
+
+        // Assert
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(CourseErrors.CourseArchived);
+
+        _courseRepositoryMock.Verify(x => x.GetCourseByIdAsync(courseId), Times.Once);
+
+        _mapperMock.Verify(x => x.Map<Lesson>(It.IsAny<LessonDTO>()), Times.Never);
+        _lessonRepositoryMock.Verify(x => x.CreateLessonAsync(It.IsAny<Lesson>()), Times.Never);
+        _mapperMock.Verify(x => x.Map<LessonResponse>(It.IsAny<Lesson>()), Times.Never);
+        _publisherMock.Verify(x => x.PublishAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        _cacheMock.Verify(x => x.RemoveAsync(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CreateLessonAsync_ShouldReturnSuccess_WhenValidData()
+    {
+        var courseId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+
+        var lessonDto = new LessonDTO();
+
+        var course = new Course
+        {
+            Id = courseId,
+            AuthorId = authorId,
+            Status = CourseStatus.Draft
+        };
+
+        var lesson = new Lesson
+        {
+            Id = Guid.NewGuid(),
+        };
+
+        var lessonResponse = new LessonResponse();
+
+        string cacheKey = "test-key";
+
+        _courseRepositoryMock
+            .Setup(x => x.GetCourseByIdAsync(courseId))
+            .ReturnsAsync(course);
+
+        _mapperMock
+            .Setup(x => x.Map<Lesson>(lessonDto))
+            .Returns(lesson);
+
+        _lessonRepositoryMock
+            .Setup(x => x.CreateLessonAsync(lesson))
+            .ReturnsAsync(lesson);
+
+        _mapperMock
+            .Setup(x => x.Map<LessonResponse>(lesson))
+            .Returns(lessonResponse);
+
+        _keyBuilderMock
+            .Setup(x => x.GetCourseKey(courseId))
+            .Returns(cacheKey);
+
+        // Act 
+        var result = await _lessonService.CreateLessonAsync(lessonDto, courseId, authorId);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Error.Should().Be(Error.None);
+        result.Value.Should().BeEquivalentTo(lessonResponse);
+
+        _courseRepositoryMock.Verify(x => x.GetCourseByIdAsync(courseId), Times.Once);
+
+        _mapperMock.Verify(x => x.Map<Lesson>(lessonDto), Times.Once);
+        _lessonRepositoryMock.Verify(x => x.CreateLessonAsync(lesson), Times.Once);
+        _mapperMock.Verify(x => x.Map<LessonResponse>(lesson), Times.Once);
+
+        _publisherMock.Verify(x => x.PublishAsync("lesson.created", It.IsAny<string>()), Times.Once);
+
+        _keyBuilderMock.Verify(x => x.GetCourseKey(courseId), Times.Once);
+        _cacheMock.Verify(x => x.RemoveAsync(cacheKey), Times.Once);
     }
 
     [Fact]
