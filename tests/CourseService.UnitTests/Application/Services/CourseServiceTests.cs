b69ebcb71b7e875a3992b010cbb36ae81854c0f0
AndriySham <avshamatrin@gmail.com>
2026-03-25 21:25:38 +0200

    Add tests to CourseService/PublishCourse

--- tests/CourseService.UnitTests/Application/Services/CourseServiceTests.cs
+++ tests/CourseService.UnitTests/Application/Services/CourseServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using CourseService.Application.Caching;
 using CourseService.Application.Courses.DTO;
+using CourseService.Application.Courses.Errors;
 using CourseService.Application.Courses.Interfaces;
 using CourseService.Application.Messaging;
+using CourseService.Domain.Abstractions;
 using CourseService.Domain.Entities;
 using CourseService.Domain.Enums;
 using FluentAssertions;
@@ -104,6 +106,113 @@ public class CourseServiceTests
                 json.Contains(authorId.ToString()))
             ), Times.Once());
     }
+
+    [Fact]
+    public async Task PublishCourseAsync_ShouldReturnFailure_WhenCourseNotFound()
+    {
+        // Arrage
+        var courseId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+
+        _courseRepositoryMock
+            .Setup(x => x.GetCourseByIdAsync(courseId))
+            .ReturnsAsync((Course?)null);
+
+        // Act 
+        var result = await _courseService.PublishCourseAsync(courseId, authorId);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(CourseErrors.CourseNotFound);
+
+        _courseRepositoryMock.Verify(x => x.GetCourseByIdAsync(courseId), Times.Once());
+
+        _courseRepositoryMock.Verify(x => x.UpdateCourseAsync(It.IsAny<Course>()), Times.Never());
+        _cacheMock.Verify(x => x.IncrementCatalogVersionAsync(), Times.Never());
+        _cacheMock.Verify(x => x.RemoveAsync(It.IsAny<string>()), Times.Never());
+        _publisherMock.Verify(x => x.PublishAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+    }
+
+    [Fact]
+    public async Task PublishCourseAsync_ShouldReturnFailure_WhenNotCourseAuthor()
+    {
+        // Arrange
+        var courseId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+
+        var anotherAuthorId = Guid.NewGuid();
+        var course = new Course
+        {
+            Id = courseId,
+            AuthorId = anotherAuthorId,
+            Status = CourseStatus.Draft
+        };
+
+        _courseRepositoryMock
+            .Setup(x => x.GetCourseByIdAsync(courseId))
+            .ReturnsAsync(course);
+
+        // Act
+        var result = await _courseService.PublishCourseAsync(courseId, authorId);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(CourseErrors.NotCourseAuthor);
+
+        _courseRepositoryMock.Verify(x => x.GetCourseByIdAsync(courseId), Times.Once());
+
+        _courseRepositoryMock.Verify(x => x.UpdateCourseAsync(It.IsAny<Course>()), Times.Never());
+        _cacheMock.Verify(x => x.IncrementCatalogVersionAsync(), Times.Never());
+        _cacheMock.Verify(x => x.RemoveAsync(It.IsAny<string>()), Times.Never());
+        _publisherMock.Verify(x => x.PublishAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+    }
+
+    [Fact]
+    public async Task PublishCourseAsync_ShouldReturnSuccess_WhenValidData()
+    {
+        // Arrange
+        var courseId = Guid.NewGuid();
+        var authorId = Guid.NewGuid();
+
+        var course = new Course
+        {
+            Id = courseId,
+            AuthorId = authorId,
+            Status = CourseStatus.Draft
+        };
+
+        var cacheKey = "test-key";
+
+        _courseRepositoryMock
+            .Setup(x => x.GetCourseByIdAsync(courseId))
+            .ReturnsAsync(course);
+
+        _keyBuilderMock
+            .Setup(x => x.GetCourseKey(courseId))
+            .Returns(cacheKey);
+
+        // Act
+        var result = await _courseService.PublishCourseAsync(courseId, authorId);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Error.Should().Be(Error.None);
+
+        course.Status.Should().Be(CourseStatus.Published);
+
+        _courseRepositoryMock.Verify(x => x.GetCourseByIdAsync(courseId), Times.Once());
+        _courseRepositoryMock.Verify(x => x.UpdateCourseAsync(course), Times.Once());   
+        _cacheMock.Verify(x => x.IncrementCatalogVersionAsync(), Times.Once());
+        _keyBuilderMock.Verify(x => x.GetCourseKey(courseId), Times.Once());
+        _cacheMock.Verify(x => x.RemoveAsync(cacheKey), Times.Once());
+
+        _publisherMock.Verify(x => x.PublishAsync("course.published",
+            It.Is<string>(json =>
+            json.Contains(courseId.ToString()) &&
+            json.Contains(authorId.ToString()))
+            ), Times.Once());
+    }
+
     //[Fact]
     //public async Task GetCourseByIdAsync_ReturnCourseResponse()
     //{
