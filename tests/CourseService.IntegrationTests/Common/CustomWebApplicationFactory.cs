73bfc528d199fcaa9fd99e1620d4134568222381
AndriySham <avshamatrin@gmail.com>
2026-04-01 16:25:44 +0300

    Off Multiplexer and Pblisher

--- tests/CourseService.IntegrationTests/Common/CustomWebApplicationFactory.cs
+++ tests/CourseService.IntegrationTests/Common/CustomWebApplicationFactory.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace CourseService.IntegrationTests.Common;
 
@@ -10,5 +13,12 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         Environment.SetEnvironmentVariable("RedisEndPoint", "localhost:6379");
         Environment.SetEnvironmentVariable("RedisUser", "test");
         Environment.SetEnvironmentVariable("RedisPassword", "test");
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<IDistributedCache>();
+
+            services.AddDistributedMemoryCache();
+        });
     }
 }
---
+++ tests/CourseService.IntegrationTests/Common/Fakes/FakeRedisKeyBuilder.cs
@@ -0,0 +1,21 @@
+﻿using CourseService.Application.Caching;
+
+namespace CourseService.IntegrationTests.Common.Fakes;
+
+public class FakeRedisKeyBuilder : IRedisKeyBuilder
+{
+    public string GetCatalogVersion()
+    {
+        return "test-catalog-version";
+    }
+
+    public string GetCourseKey(Guid courseId)
+    {
+        return $"test-course-{courseId}";
+    }
+
+    public string GetCoursesPageKey(long version, int page, int size)
+    {
+        return $"test-courses-{version}-{page}-{size}";
+    }
+}
