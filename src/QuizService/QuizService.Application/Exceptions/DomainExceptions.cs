d59153564ae9df2fb53b822d4161d6472421d233
Choffel <daniilmiadzve@gmail.com>
2026-05-05 19:14:02 +0300

    feat: add domain exceptions for event publishing - Add ApplicationException base class - Add EventPublishingException for Redis failures - Add AttemptNotFoundException for missing attempts

---
+++ src/QuizService/QuizService.Application/Exceptions/DomainExceptions.cs
@@ -0,0 +1,29 @@
+﻿namespace QuizService.Application.Exceptions;
+
+/// <summary>
+/// Базовое исключение приложения
+/// </summary>
+public class ApplicationException : Exception
+{
+    public ApplicationException(string message) : base(message) { }
+    public ApplicationException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+/// <summary>
+/// Исключение при неудаче публикации события
+/// </summary>
+public class EventPublishingException : ApplicationException
+{
+    public EventPublishingException(string streamKey, Exception innerException)
+        : base($"Failed to publish event to stream '{streamKey}'", innerException) { }
+}
+
+/// <summary>
+/// Исключение при попытке завершить несуществующую попытку
+/// </summary>
+public class AttemptNotFoundException : ApplicationException
+{
+    public AttemptNotFoundException(Guid attemptId)
+        : base($"Quiz attempt with ID '{attemptId}' not found") { }
+}
+
