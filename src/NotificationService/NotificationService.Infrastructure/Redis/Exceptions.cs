0f06e31aec6c94885645362647ebacb69f74021d
Choffel <daniilmiadzve@gmail.com>
2026-05-05 19:20:29 +0300

    feat(notification): add Redis stream subscriber infrastructure

---
+++ src/NotificationService/NotificationService.Infrastructure/Redis/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿namespace NotificationService.Infrastructure.Redis;
+
+
+public class StreamProcessingException : Exception
+{
+    public StreamProcessingException(string message) : base(message) { }
+    public StreamProcessingException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+
+public class ConsumerGroupInitializationException : Exception
+{
+    public ConsumerGroupInitializationException(string groupName, string streamKey, Exception innerException)
+        : base($"Failed to initialize consumer group '{groupName}' for stream '{streamKey}'", innerException) { }
+}
+
