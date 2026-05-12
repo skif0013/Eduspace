faa906d977774b8d42a2341f673d974ded855d68
skif <113833988+skif0013@users.noreply.github.com>
2026-05-12 13:08:38 +0200

    Remove AuthService and UserService projects

--- src/UserService/UserService.Application/DTO/UpdateUserDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace UserService.Application.DTO;
-
-public class UpdateUserDTO
-{
-    public string UserName { get; set; }
-    public string Email { get; set; }
-    public string AvatarURL { get; set; }
-}
