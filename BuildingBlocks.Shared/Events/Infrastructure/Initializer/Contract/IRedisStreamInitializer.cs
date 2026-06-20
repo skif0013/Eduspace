f7d20a0c8bc266471e87e547c36b19983a9bf04a
Choffel <daniilmiadzve@gmail.com>
2026-06-21 01:06:06 +0200

    infra: add RedisStreamInitializer for managing consumer groups

---
+++ BuildingBlocks.Shared/Events/Infrastructure/Initializer/Contract/IRedisStreamInitializer.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Shared.Events.Infrastructure.Initializer.Contract;
+
+public interface IRedisStreamInitializer
+{
+    Task EnsureGroupExists(string groupName, string streamKey);
+    
+    Task CreateGroupASync(string groupName, string streamKey);
+}
