61cd6108340a258a7d05f57276d80ca2af50355f
Choffel <daniilmiadzve@gmail.com>
2026-07-01 09:07:54 +0200

    feat(building-shared): add event contract to start quiz

---
+++ BuildingBlocks.Shared/Events/Messaging/Contracts/Quizzes/QuizStartedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Redis.Events;
+
+public record QuizStartedEvent(
+    Guid AttemptId,
+    string UserEmail,
+    string QuizId) : IntegrationEvent(
+    Guid.NewGuid(),
+    DateTime.UtcNow);
