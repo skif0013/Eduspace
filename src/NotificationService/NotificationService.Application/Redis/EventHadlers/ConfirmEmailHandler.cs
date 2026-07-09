e9f8302443de7ba1e374ec86e50783fd5578faa7
Choffel <daniilmiadzve@gmail.com>
2026-07-09 21:39:48 +0200

    chore(email-handler) clean-up email handler

--- src/NotificationService/NotificationService.Application/Redis/EventHadlers/ConfirmEmailHandler.cs
+++ src/NotificationService/NotificationService.Application/Redis/EventHadlers/ConfirmEmailHandler.cs
@@ -14,25 +14,22 @@ public class ConfirmEmailHandler : ScopedMessageHandler<EmailVerifyEvent>
     {
     }
 
-    // Имя канала строго совпадает с тем, что мы записали в Outbox Type ("email:verify")
+    
     public override string Channel => "email:verify";
     
-    // Метод принимает уже готовый, десериализованный объект @event
+    
     protected override async Task HandleMessageAsync(EmailVerifyEvent @event, IServiceProvider serviceProvider)
     {
-        // Достаем из локального scope сервис отправки писем
         var emailService = serviceProvider.GetRequiredService<IEmailService>();
         
-        // Маппим данные из ивента в DTO для отправки
         var emailDTO = new EmailVerifyDTO()
         {
             To = @event.To,
             UserName = @event.UserName,
-            VerificationLink = @event.VerificationLink, // Если ссылка генерируется на стороне Identity
+            VerificationLink = @event.VerificationLink, 
             Code = @event.Code
         };
-
-        // Отправляем письмо
+        
         await emailService.SendVerifyEmailAsync(emailDTO);
     }
 }
