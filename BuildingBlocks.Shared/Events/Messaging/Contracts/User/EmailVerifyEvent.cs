cdadce4488bd66eeb288acc4fa59e8bbae5a5548
Choffel <daniilmiadzve@gmail.com>
2026-07-09 21:25:02 +0200

    feat(shared): add EmailVerifyEvent integration contract

---
+++ BuildingBlocks.Shared/Events/Messaging/Contracts/User/EmailVerifyEvent.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Redis.Events;
+
+namespace BuildingBlocks.Shared.Events.Messaging.Contracts.User;
+
+public record EmailVerifyEvent(
+    string To,
+    string UserName,
+    string Code
+    ) : IntegrationEvent(new Guid(), DateTime.UtcNow);
