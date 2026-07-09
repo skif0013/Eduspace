c115ddd83cafece6366656a4974759c455115b0a
Choffel <daniilmiadzve@gmail.com>
2026-07-09 21:25:20 +0200

    chore(notification): clean up obsolete handler and update application csproj

--- src/NotificationService/NotificationService.Application/DTOs/EmailVerifyDTO.cs
+++ src/NotificationService/NotificationService.Application/DTOs/EmailVerifyDTO.cs
@@ -5,7 +5,6 @@ public class EmailVerifyDTO
     public string To { get; set; }
     
     public string UserName { get; set; }
-    
     public string VerificationLink { get; set; }
     public string Code { get; set; }
 }
