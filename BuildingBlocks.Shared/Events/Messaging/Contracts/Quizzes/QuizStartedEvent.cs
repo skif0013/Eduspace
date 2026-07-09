8dbc020a6c406927c43cd33e90766a6a2788ecc6
Choffel <daniilmiadzve@gmail.com>
2026-07-09 21:24:58 +0200

    chore(shared): remove obsolete quiz events and update domain models

--- BuildingBlocks.Shared/Events/Messaging/Contracts/Quizzes/QuizStartedEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BuildingBlocks.Redis.Events;
-
-public record QuizStartedEvent(
-    Guid AttemptId,
-    string UserEmail,
-    string QuizId) : IntegrationEvent(
-    Guid.NewGuid(),
-    DateTime.UtcNow);
