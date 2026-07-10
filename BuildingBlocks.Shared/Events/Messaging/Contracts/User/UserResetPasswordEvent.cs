278842556309c33f63768434baf5bc03cc4b5d9d
Choffel <daniilmiadzve@gmail.com>
2026-07-10 19:17:58 +0200

    feature: add reset-password dto

--- BuildingBlocks.Shared/Events/Messaging/Contracts/User/UserResetPasswordEvent.cs
+++ BuildingBlocks.Shared/Events/Messaging/Contracts/User/UserResetPasswordEvent.cs
@@ -2,6 +2,6 @@ namespace BuildingBlocks.Redis.Events;
 
 public class UserResetPasswordEvent
 {
-    public string UserEmail { get; set; }
+    public string To { get; set; }
     public string Token { get; set; }
 }
