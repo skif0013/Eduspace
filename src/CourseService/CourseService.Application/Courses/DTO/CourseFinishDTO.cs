b01503b632b79e120ad05006a154c9497e7e8d75
Choffel <daniilmiadzve@gmail.com>
2026-07-10 20:33:10 +0200

    feat(course,notification): FinishDto and handler

---
+++ src/CourseService/CourseService.Application/Courses/DTO/CourseFinishDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseService.Application.Courses.DTO;
+
+public record CourseFinishDTO(
+    string To,
+    string UserName,
+    Guid CourseId,
+    string CourseTitle,
+    DateTime FinishDate,
+    decimal FinishScore
+    );
---
+++ src/NotificationService/NotificationService.Application/Redis/EventHadlers/CourseFinishHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.Redis.Events.Handler;
+using BuildingBlocks.Shared.Events.Messaging.Contracts.Course;
+using CourseService.Application.Courses.DTO;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Application.Contracts;
+
+namespace NotificationService.Application.Redis.EventHadlers;
+
+public class CourseFinishHandler : ScopedMessageHandler<CourseFinishEvent>
+{
+    public CourseFinishHandler(IServiceScopeFactory scopeFactory) : base(scopeFactory)
+    {
+    }
+
+    public override string Channel => "course:finish";
+    protected override  async Task HandleMessageAsync(CourseFinishEvent @event, IServiceProvider serviceProvider)
+    {
+        var emailService =   serviceProvider.GetRequiredService<IEmailService>();
+        
+        var emailDto  = new CourseFinishDTO(@event.UserEmail, @event.UserName, @event.CourseId, @event.CourseTitle, @event.FinishDate, @event.FinishScore);
+        
+        
+        await emailService.SendFinishCoursMailAsync(emailDto);
+    }
+}
