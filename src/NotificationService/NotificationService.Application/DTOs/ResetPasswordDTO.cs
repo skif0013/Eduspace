87eda4ddaa45369771cedd952cadd94291c74f5d
Choffel <daniilmiadzve@gmail.com>
2026-07-10 19:20:05 +0200

    feature: add handler and dto for reset password

---
+++ src/NotificationService/NotificationService.Application/DTOs/ResetPasswordDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace NotificationService.Application.DTOs;
+
+public record ResetPasswordDTO(
+    string To,
+    string Token
+    );
---
+++ src/NotificationService/NotificationService.Application/Redis/EventHadlers/ResetUserPasswordHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.Redis.Events;
+using BuildingBlocks.Redis.Events.Handler;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Application.Contracts;
+using NotificationService.Application.DTOs;
+
+namespace NotificationService.Application.Redis.EventHadlers;
+
+public class ResetUserPasswordHandler : ScopedMessageHandler<UserResetPasswordEvent>
+{
+    public ResetUserPasswordHandler(IServiceScopeFactory scopeFactory) : base(scopeFactory)
+    {
+    }
+
+
+    public override string Channel => "user:reset:password";
+    
+    protected override Task HandleMessageAsync(UserResetPasswordEvent @event, IServiceProvider serviceProvider)
+    {
+        var emailService = serviceProvider.GetRequiredService<IEmailService>();
+
+        var emailDTO = new ResetPasswordDTO(
+            @event.To,
+            @event.Token
+        );
+        
+        return emailService.SendResetPasswordEmailAsync(emailDTO);
+    }
+}
